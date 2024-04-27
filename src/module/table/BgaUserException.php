03ec183bd0a6ea2b16f393eb57270d72c3f81154
Kevin Kelley <kelleyk@kelleyk.net>
2024-04-27 09:04:52 -0700

    Add comments describing exception types

--- src/module/table/BgaUserException.php
+++ src/module/table/BgaUserException.php
@@ -1,5 +1,8 @@
 <?php
 
+// `BgaUserException` should be used for situations where nothing
+// technical has gone wrong, but e.g. the user has done something
+// invalid.
 class BgaUserException extends Exception
 {
   function __construct($message = null, $code = 0, Exception $previous = null)
--- src/module/table/BgaVisibleSystemException.php
+++ src/module/table/BgaVisibleSystemException.php
@@ -1,5 +1,8 @@
 <?php
 
+// `BgaVisibleSystemException` should be used when something internal
+// has gone wrong.  In the rare cases where the message might contain
+// sensitive information, use `BgaSystemException` instead.
 class BgaVisibleSystemException extends Exception
 {
   function __construct($message = null, $code = 0, Exception $previous = null)
